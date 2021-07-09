0d8be3c5a38e9831cd3fc9e1cfaa43a72dd6c848
Facundo Murga2 <facumurga.95@gmail.com>
2021-07-09 10:48:33 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { header } from './components/Header/header';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss'
+
+export const header = () => {
+    return (
+        <header>
+            
+        </header>
+    )
+}
